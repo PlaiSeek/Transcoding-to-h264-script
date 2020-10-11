4f86e3376e1d6ea1e2cb69ea7732aceb1d6b6617
PlaiSeek <francois.hamonic@gmail.com>
2020-10-11 19:01:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+install HandbrakeCLI from ppa (https://launchpad.net/~stebbins/+archive/ubuntu/handbrake-releases) : 
+    sudo add-apt-repository ppa:stebbins/handbrake-releases
+    sudo apt update
+    sudo apt install handbrake-cli
---
+++ transcode_x264.py
@@ -0,0 +1,81 @@
+import os
+import subprocess
+import sys
+import time
+
+def list_non_h264(dir, extensions=[".mkv", ".avi", ".mp4", ".m4v"]):
+    file_paths = []
+    for filename in os.listdir(dir):
+        if os.path.splitext(filename)[1] not in extensions: continue
+        file_path = dir + "/" + filename
+        cmd=["ffprobe", "-i", file_path, "-v", "error", "-of", "default=noprint_wrappers=1:nokey=1", "-show_entries", "stream=codec_name"]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, _ = p.communicate()
+        if p.returncode != 0:
+            raise subprocess.CalledProcessError(p.returncode, p.args)
+        codecs = stdout.decode("utf-8").split("\n")
+        if "h264" in codecs: continue            
+        file_paths.append(filename)
+    # for file_path in file_paths:
+    #     print(file_path)
+    # print(len(file_paths))
+    return file_paths
+
+progress_bar_width = 0
+def print_progress_bar(line):
+    global progress_bar_width
+    line = line.split('\n')[0]
+    sys.stdout.write("\b" * progress_bar_width)
+    sys.stdout.write(" " * progress_bar_width)
+    sys.stdout.write("\b" * progress_bar_width)
+    sys.stdout.write(line)
+    progress_bar_width = len(line)
+    sys.stdout.flush()
+
+
+preset_file="Jellyfin 1080p MKV x264.json"
+in_dir="/media/plaiseek/2To Storage/Films"
+out_dir="/media/plaiseek/2To Storage/Films x264"
+out_extension=".mkv"
+
+
+if not os.path.exists(out_dir):
+    try: os.mkdir(out_dir)
+    except OSError: print("Creation of the directory {} failed".format(out_dir))
+    else: print("Successfully created the directory {}".format(out_dir))
+
+print("Listing non h264 files in \"{}\"".format(in_dir))
+filenames = list_non_h264(in_dir)
+nb_files = len(filenames)
+print("{} files found".format(nb_files))
+
+start_time = time.time()
+print("Transcoding ...")
+for i, filename in enumerate(filenames):
+    in_file = in_dir + "/" + filename
+    out_file = out_dir + "/" + os.path.splitext(filename)[0] + out_extension
+
+    print("({}/{}) \"{}\"".format(i+1, nb_files, out_file))
+
+    if os.path.exists(out_file): continue
+
+    cmd=["HandBrakeCLI", "--preset-import-file", preset_file, "-a", "1,2,3", "-s", "1,2,3,4,5,6", "-i", in_file, "-o", out_file]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    
+    for stdout_line in iter(p.stdout.readline, ""):
+        print_progress_bar(stdout_line)
+    print_progress_bar("")
+
+    stdout, stderr = p.communicate()
+
+    if p.returncode != 0:
+        with open("log_stdout", "w") as log_out:
+            log_out.write(stdout)
+        with open("log_stderr", "w") as log_err:
+            log_err.write(stderr)
+        raise subprocess.CalledProcessError(p.returncode, p.args)
+
+end_time = time.time()
+hours, rem = divmod(end_time-start_time, 3600)
+minutes, seconds = divmod(rem, 60)
+print("Completed in {:0>2} hours {:0>2} minutes and {:05.2f} seconds !".format(int(hours),int(minutes),seconds))
